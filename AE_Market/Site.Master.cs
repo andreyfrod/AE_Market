20c0329a1a1fe0388bab3ecf379afecd7a48f24b
Andrey Rodriguez <andreyfrodriguez@hotmail.com>
2020-03-22 16:24:20 -0600

    Add project files.

---
+++ AE_Market/Site.Master.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AE_Market
+{
+    public partial class SiteMaster : MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["user"] != null)
+            {
+                lblNombreUsuario.Visible = true;
+                linkLogout.Visible = true;
+
+                UserEntidad user = (UserEntidad)Session["user"];
+                lblNombreUsuario.Text = user.nombreUser;
+
+            }
+            else
+            {
+                Response.Redirect("login.aspx");
+            }
+        }
+    }
+}
