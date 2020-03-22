d1d105076ce2e8bd9cb7b67d0f11f07e20ae0c3c
Andrey Rodriguez <andreyfrodriguez@hotmail.com>
2020-03-22 17:59:48 -0600

    Creacion entidad Usuario

---
+++ Entidades/UsuarioEntidad.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class UsuarioEntidad
+    {
+        public int idUsuario { get; set; }
+        public String tipoUsuario { get; set; }
+        public String nombre { get; set; }
+        public String nivel { get; set; }
+        public String email { get; set; }
+        public String password { get; set; }
+    }
+}
