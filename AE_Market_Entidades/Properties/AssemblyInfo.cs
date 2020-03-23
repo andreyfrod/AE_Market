72fb8e2a38571bf499e6103e2f2c06ab7b806373
Andrey Rodriguez <andreyfrodriguez@hotmail.com>
2020-03-22 18:38:04 -0600

    Cambio de nombres 2

--- AE_Market_Entidades/Properties/AssemblyInfo.cs
+++ AE_Market_Entidades/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Entidades")]
+[assembly: AssemblyTitle("AE_Market_Entidades")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Entidades")]
+[assembly: AssemblyProduct("AE_Market_Entidades")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
